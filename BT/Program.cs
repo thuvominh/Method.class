e93913c5c9f5d4e9e798610c50ff97240206d40c
thuvominh <thuvtmgcd210164@fpt.edu.vn>
2021-11-23 23:03:49 +0700

    done

---
+++ BT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] SoNguyen = {1, 4, 5, 2, 3, 4, 2, 1, 1, 6, 7, 7};
+            // int sum = 0;
+            //
+            // foreach (var element in SoNguyen)
+            // {
+            //     sum += element;
+            // }
+            // Console.WriteLine(sum);
+
+            int[] SoNguyen = {1, 4, 5, 2, 3, 4, 2, 1, 1, 6, 7, 7};
+
+            for (int i = 0; i < SoNguyen.Length; i++)
+            {
+                Console.WriteLine(SoNguyen[i]);
+            }
+
+
+
+        }
+    }
+}
+    
